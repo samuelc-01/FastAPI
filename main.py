a9b8febe78c11b0c59bdad9e0373e838b642e9f6
dev <94959646+samuelc-01@users.noreply.github.com>
2025-06-13 17:28:58 -0300

    create db and create two users

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
 
+from models import User, Gender, Role
+
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Samuel",
+        last_name="Cristian",
+        gender=Gender.male,
+        roles=[Role.student]      
+    ),
+    User(
+        id=uuid4(),
+        first_name="Jean",
+        last_name="Cristian",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]      
+    ),
+]
 
 @app.get("/")
 def root():
