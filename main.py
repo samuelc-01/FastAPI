8754fea8878f98acb9e68ffe5cd992cfa395e548
dev <94959646+samuelc-01@users.noreply.github.com>
2025-06-18 10:33:18 -0300

    feat: update fucntion add

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 from uuid import UUID
 from fastapi import FastAPI, HTTPException
 
-from models import User, Gender, Role
+from models import User, Gender, Role, UserUpdateRequest
 
 #py -m uvicorn main:app --reload
 #lh:yourport/docs for swagger
@@ -47,6 +47,25 @@ async def delete_user(user_id: UUID):
         if user.id == user_id:
             db.remove(user)
             return 
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
+
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
     raise HTTPException(
         status_code=404,
         detail=f"user with id: {user_id} does not exists"
